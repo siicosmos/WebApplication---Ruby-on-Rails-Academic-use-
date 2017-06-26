cb61a469a61698b38297204ccaf1b6a1e2e79a70
rthanki <rthanki@sfu.ca>
2017-06-25 20:18:54 -0700

    not able to change the title color

--- Pizza-Cluster/config/routes.rb
+++ Pizza-Cluster/config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 	get 'menu' => 'menu#show'
 
  	get 'order' => 'order#show'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
