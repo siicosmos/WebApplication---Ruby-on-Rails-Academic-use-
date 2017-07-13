447bfae5cd3e4dbd1518aef8ed54c3254171efd4
Liam Ling <kla121@sfu.ca>
2017-07-12 23:30:17 -0700

    update

--- Pizza-Cluster/config/application.rb
+++ Pizza-Cluster/config/application.rb
@@ -10,6 +10,7 @@ module PizzaCluster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.precompile += %w[ active_admin.css active_admin.js ]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- Pizza-Cluster/config/environments/development.rb
+++ Pizza-Cluster/config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
