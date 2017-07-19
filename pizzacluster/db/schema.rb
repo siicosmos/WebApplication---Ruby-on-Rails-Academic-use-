5dc5ecb4c1e0016f77afe2bc7a6958e70dfc0907
rthanki <rthanki@sfu.ca>
2017-07-18 22:45:03 -0700

    fixed the error I was having running rails server on Windows

--- pizzacluster/db/schema.rb
+++ pizzacluster/db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714092442) do
-
-  create_table "sessions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 20170714033333) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
